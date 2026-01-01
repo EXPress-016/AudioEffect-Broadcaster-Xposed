b03404126c8feff1490264ea445da2f5b0e41a1b
EXPress-016 <74526510+EXPress-016@users.noreply.github.com>
2026-01-01 19:55:12 +0530

    feat: Support AudioTrack

--- app/src/main/kotlin/me/express016/audioeffectbroadcasterxposed/Hook.kt
+++ app/src/main/kotlin/me/express016/audioeffectbroadcasterxposed/Hook.kt
@@ -1,7 +1,8 @@
 package me.express016.audioeffectbroadcasterxposed
 
-import android.content.Context
+import android.app.AndroidAppHelper
 import android.content.Intent
+import android.media.AudioTrack
 import android.media.audiofx.AudioEffect
 import android.util.Log
 import androidx.annotation.Keep
@@ -11,7 +12,6 @@ import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
-
 
 
 @Keep
@@ -42,7 +42,10 @@ class AudioEffectBroadcasterHook : IXposedHookLoadPackage {
 
 
         // hookOnCreate(lpparam)
-        hookExoPlayerImpl(lpparam)
+
+        val hookedExoPlayerImpl = hookExoPlayerImpl(lpparam)
+
+        if (!hookedExoPlayerImpl) hookAudioTrack(lpparam)
 
         Log.d(TAG, "Hooked ${lpparam.packageName}")
     }
@@ -57,13 +60,67 @@ class AudioEffectBroadcasterHook : IXposedHookLoadPackage {
 //            })
 //    }
 
-    private fun hookExoPlayerImpl(lpparam: XC_LoadPackage.LoadPackageParam) {
+    private fun hookAudioTrack(lpparam: XC_LoadPackage.LoadPackageParam) {
+
+        try {
+            val audioTrack =
+                XposedHelpers.findClass("android.media.AudioTrack", lpparam.classLoader)
+
+            Log.d(TAG, "Hooking ${audioTrack.packageName}.${audioTrack.name}")
+
+
+            XposedHelpers.findAndHookMethod(
+                audioTrack, "play", object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: MethodHookParam?) {
+                        val track = param?.thisObject as AudioTrack?
+
+                        track?.let {
+                            if (track.playState != AudioTrack.PLAYSTATE_PLAYING) return@let param.result
+
+                            val audioSessionId = track.audioSessionId
+
+                            broadcastAudioEffectAction(
+                                audioSessionId, AudioEffect.ACTION_OPEN_AUDIO_EFFECT_CONTROL_SESSION
+                            )
+
+                        }
+
+                    }
+                })
+
+
+            arrayOf("pause", "stop", "release", "flush").forEach {
+                XposedHelpers.findAndHookMethod(
+                    audioTrack, it, object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam?) {
+                            val track = param?.thisObject as AudioTrack?
+
+                            track?.let {
+                                if (track.playState == AudioTrack.PLAYSTATE_PLAYING) return@let param.result
+
+                                val audioSessionId = track.audioSessionId
+
+                                broadcastAudioEffectAction(
+                                    audioSessionId,
+                                    AudioEffect.ACTION_CLOSE_AUDIO_EFFECT_CONTROL_SESSION
+                                )
+
+                            }
+                        }
+                    })
+            }
+        } catch (e: Throwable) {
+            Log.e(TAG, "hookAudioTrack Error", e)
+        }
+    }
+
+    private fun hookExoPlayerImpl(lpparam: XC_LoadPackage.LoadPackageParam): Boolean {
         try {
             val exoPlayerImpl = findExoPlayerImplClass(lpparam.classLoader)
 
             if (exoPlayerImpl == null) {
-                Log.d(TAG, "No compatible ExoPlayer class found.")
-                return
+                Log.d(TAG, "No compatible ExoPlayer class found, trying android.media.AudioTrack.")
+                return false
             }
 
             Log.d(TAG, "Hooking ${exoPlayerImpl.packageName}.${exoPlayerImpl.name}")
@@ -86,13 +143,19 @@ class AudioEffectBroadcasterHook : IXposedHookLoadPackage {
                                 AudioEffect.ACTION_CLOSE_AUDIO_EFFECT_CONTROL_SESSION
                             }
 
-                            broadcastAudioEffectAction(playerInstance, action)
+                            val audioSessionId = XposedHelpers.callMethod(
+                                playerInstance, "getAudioSessionId"
+                            ) as Int? ?: return
+
+                            broadcastAudioEffectAction(audioSessionId, action)
                         }
                     }
                 })
 
+            return true
         } catch (e: Throwable) {
             Log.e(TAG, "hookExoPlayerImpl Error", e)
+            return false
         }
     }
 
@@ -100,7 +163,7 @@ class AudioEffectBroadcasterHook : IXposedHookLoadPackage {
         val classNames = listOf(
             "androidx.media3.exoplayer.ExoPlayerImpl",
             "com.google.android.exoplayer2.ExoPlayerImpl",
-                    )
+        )
 
         for (className in classNames) {
             try {
@@ -112,14 +175,10 @@ class AudioEffectBroadcasterHook : IXposedHookLoadPackage {
         return null
     }
 
-    private fun broadcastAudioEffectAction(playerInstance: Any, event: String) {
-        val audioSessionId =
-            XposedHelpers.callMethod(playerInstance, "getAudioSessionId") as Int? ?: return
-
-        if (audioSessionId == -1) return
-
-        val targetContext =
-            XposedHelpers.getObjectField(playerInstance, "applicationContext") as? Context ?: return
+    private fun broadcastAudioEffectAction(
+        audioSessionId: Int, event: String
+    ) {
+        val targetContext = AndroidAppHelper.currentApplication()
 
         Log.d(
             TAG,
